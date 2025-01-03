01e2e900290bffdd8f2bb142385545015e0136b5
Reyn <jl88744653@gmail.com>
2025-01-03 23:44:26 +0800

    支持更多加载方式
    支持从本地动态加载、远程加载 loadRemote、远程加载 loadAny

--- assets/src/ASTC.ts
+++ assets/src/ASTC.ts
@@ -1,34 +1,69 @@
 import { gfx, director, Asset, ImageAsset, SpriteFrame } from "cc";
 
 export namespace ASTC {
-    /** 目标纹理格式 */
-    export const TARGET_FORMAT = gfx.Format.ASTC_RGBA_6X6;
+  /** 目标纹理格式 */
+  export const TARGET_FORMAT = gfx.Format.ASTC_RGBA_6X6;
 
-    /**
-     * 是否支持指定的纹理格式
-     * @param format 纹理格式
-     * @returns
-     */
-    export function IsFormatSuported(format: gfx.Format) {
-        return (director.root.device.getFormatFeatures(format) & gfx.FormatFeatureBit.SAMPLED_TEXTURE) > 0;
-    }
+  /**
+   * 是否支持指定的纹理格式
+   * @param format 纹理格式
+   * @returns
+   */
+  export function IsFormatSuported(format: gfx.Format) {
+    return (
+      (director.root.device.getFormatFeatures(format) &
+        gfx.FormatFeatureBit.SAMPLED_TEXTURE) >
+      0
+    );
+  }
 
-    /**
-     * 平台是否支持目标纹理格式
-     * @returns
-     */
-    export function IsPlatformSupported() {
-        return IsFormatSuported(TARGET_FORMAT);
-    }
+  /**
+   * 平台是否支持目标纹理格式
+   * @returns
+   */
+  export function IsPlatformSupported() {
+    return IsFormatSuported(TARGET_FORMAT);
+  }
 
-    /**
-     * 创建精灵帧
-     * @param asset 资源
-     */
-    export function CreateSpriteFrame(asset: Asset) {
-        const file = (<any>asset)._file;
-        const data: Uint8Array = file._data;
-        const image = (<any>ImageAsset).parseCompressedTextures(data.buffer, 2);
-        return SpriteFrame.createWithImage(image);
+  /**
+   * 创建精灵帧
+   * @param asset 资源
+   */
+  export function CreateSpriteFrameFromRemote(
+    asset: Asset & {
+      _nativeData?: CompressedTextureInfo;
     }
+  ) {
+    if (asset._nativeData) {
+      return CreateSpriteFrameByInfo(asset._nativeData);
+    }
+    return null;
+  }
+
+  export function CreateSpriteFrameFromDynamic(
+    asset: Asset & {
+      _file?: CompressedTextureInfo;
+    }
+  ) {
+    if (asset._file) {
+      return CreateSpriteFrameByInfo(asset._file);
+    }
+    return null;
+  }
+
+  export function CreateSpriteFrameByInfo(info: CompressedTextureInfo) {
+    const image = (<any>ImageAsset).parseCompressedTextures(
+      info._data.buffer,
+      2
+    );
+    return SpriteFrame.createWithImage(image);
+  }
+}
+
+interface CompressedTextureInfo {
+  _compressed: boolean;
+  _data: Uint8Array;
+  format: gfx.Format;
+  width: number;
+  height: number;
 }
--- assets/src/ASTCLoader.ts
+++ assets/src/ASTCLoader.ts
@@ -1,31 +1,87 @@
-import { _decorator, BufferAsset, Color, Component, gfx, ImageAsset, Label, resources, Sprite, SpriteFrame } from "cc";
+import {
+  _decorator,
+  assetManager,
+  BufferAsset,
+  Color,
+  Component,
+  gfx,
+  ImageAsset,
+  Label,
+  resources,
+  Sprite,
+  SpriteFrame,
+} from "cc";
 import { ASTC } from "./ASTC";
 const { ccclass, property } = _decorator;
 const EXTENSIONS = { PNG: "CE_PNG", ASTC: "CE_ASTC" };
 
 @ccclass("ASTCLoader")
 export class ASTCLoader extends Component {
-    @property(Sprite)
-    sprite: Sprite = null;
+  @property(Sprite)
+  sprite: Sprite = null;
 
-    @property(Label)
-    title: Label = null;
+  @property(Label)
+  title: Label = null;
 
-    start() {
-        if (ASTC.IsPlatformSupported()) {
-            this.title.string = "ASTC is supported on this platform.";
-            this.title.color = Color.GREEN;
-            resources.load(EXTENSIONS.ASTC, (err, asset) => {
-                if (err) return console.error(err);
-                this.sprite.spriteFrame = ASTC.CreateSpriteFrame(asset);
-            });
-        } else {
-            this.title.string = "ASTC is not supported on this platform!";
-            this.title.color = Color.RED;
-            resources.load(EXTENSIONS.PNG + "/spriteFrame", SpriteFrame, (err, asset) => {
-                if (err) return console.error(err);
-                this.sprite.spriteFrame = asset;
-            });
-        }
+  start() {
+    if (ASTC.IsPlatformSupported()) {
+      this.title.string = "ASTC is supported on this platform.";
+      this.title.color = Color.GREEN;
+      this.dynamicLoadAstc();
+      setTimeout(() => {
+        this.remoteLoadAstc();
+      }, 2000);
+      setTimeout(() => {
+        this.anyLoadAstc();
+      }, 4000);
+    } else {
+      this.title.string = "ASTC is not supported on this platform!";
+      this.title.color = Color.RED;
+      this.dynamicLoadImage();
     }
+  }
+
+  private anyLoadAstc() {
+    assetManager.loadAny(
+      { url: "http://192.168.1.172:3700/astc/CE_ASTC.astc" },
+      (err, data) => {
+        if (err) return console.error(err);
+        console.log("远程", data);
+        this.sprite.spriteFrame = ASTC.CreateSpriteFrameByInfo(data);
+        this.title.string = "loadAny 创建成功！";
+      }
+    );
+  }
+
+  private remoteLoadAstc() {
+    assetManager.loadRemote(
+      "http://192.168.1.172:3700/astc/CE_ASTC.astc",
+      (err, data) => {
+        if (err) return console.error(err);
+        console.log("远程", data);
+        this.sprite.spriteFrame = ASTC.CreateSpriteFrameFromRemote(data);
+        this.title.string = "loadRemote 创建成功！";
+      }
+    );
+  }
+
+  private dynamicLoadImage() {
+    resources.load(
+      EXTENSIONS.PNG + "/spriteFrame",
+      SpriteFrame,
+      (err, asset) => {
+        if (err) return console.error(err);
+        this.sprite.spriteFrame = asset;
+      }
+    );
+  }
+
+  private dynamicLoadAstc() {
+    resources.load(EXTENSIONS.ASTC, (err, asset) => {
+      if (err) return console.error(err);
+      console.log("动态", asset);
+      this.title.string = "resources 创建成功！";
+      this.sprite.spriteFrame = ASTC.CreateSpriteFrameFromDynamic(asset);
+    });
+  }
 }
